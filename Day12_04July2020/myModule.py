d9b18a9155d8617f687ac83094d426f60fefceca
Anil Kumar Teegala <teegala.anilkumar1@gmail.com>
2020-07-04 17:00:31 +0530

    Day12 Content added

---
+++ Day12_04July2020/myModule.py
@@ -0,0 +1,15 @@
+def even(number):
+    if number % 2 == 0:
+        return True
+    else:
+        return False
+    
+def factorial(number):
+    fact = 1
+    for i in range(1, number + 1):
+        fact *= i
+    return fact
+
+pi = 3.14
+
+x = 5
